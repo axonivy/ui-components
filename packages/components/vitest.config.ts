df96552c3ac3e0b14a3e404d93d732eb06bbec97
Lukas Lieb <lukas.lieb@axonivy.com>
2024-01-11 14:01:41 +0100

    Add components package

---
+++ packages/components/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  test: {
+    dir: 'src',
+    include: ['**/*.test.ts?(x)'],
+    alias: {
+      '@axonivy/editor-icons': resolve(__dirname, '../../packages/icons/src-gen'),
+      'test-utils': resolve(__dirname, 'src/test-utils/test-utils.tsx')
+    },
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-utils/setupTests.tsx'],
+    css: false,
+    reporters: process.env.CI ? ['basic', 'junit'] : ['default'],
+    outputFile: 'report.xml'
+  }
+});
