7aba89e8f1524883e0c767648821f5e0889b9be7
Lukas Lieb <lukas.lieb@axonivy.com>
2024-02-27 12:52:13 +0100

    Fix raise script

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
 
-yarn install --ignore-scripts
-yarn lerna version ${1/SNAPSHOT/next} --no-git-tag-version --no-push --ignore-scripts --exact --yes
+npm install
+sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" packages/*/package.json
+npx lerna version ${1/SNAPSHOT/next} --no-git-tag-version --no-push --ignore-scripts --exact --yes
+npm install
