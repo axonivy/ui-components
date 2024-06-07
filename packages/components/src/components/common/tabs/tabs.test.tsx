a4d8cfb0f76ba2a5ded0c997fcd253e79348ccc5
Lukas Lieb <lukas.lieb@axonivy.com>
2024-06-07 15:43:14 +0200

    Add tabs component

---
+++ packages/components/src/components/common/tabs/tabs.test.tsx
@@ -0,0 +1,37 @@
+import { composeStory } from '@storybook/react';
+import { act, render, screen, userEvent } from 'test-utils';
+import { expect, test } from 'vitest';
+import Meta, { Default } from './tabs.stories';
+
+const Tabs = composeStory(Default, Meta);
+
+test('toggle', async () => {
+  render(<Tabs />);
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Attribute list');
+  const func = screen.getByRole('tab', { name: /Function/ });
+  await act(async () => await userEvent.click(func));
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Function list');
+
+  const attr = screen.getByRole('tab', { name: /Attribute/ });
+  await act(async () => await userEvent.click(attr));
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Attribute list');
+});
+
+test('toggled with keyboard', async () => {
+  render(<Tabs />);
+
+  const attr = screen.getByRole('tab', { name: /Attribute/ });
+  const func = screen.getByRole('tab', { name: /Function/ });
+
+  await act(async () => await userEvent.tab());
+  expect(attr).toHaveFocus();
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Attribute list');
+
+  await act(async () => await userEvent.keyboard('[ArrowRight]'));
+  expect(func).toHaveFocus();
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Function list');
+
+  await act(async () => await userEvent.keyboard('[ArrowLeft]'));
+  expect(attr).toHaveFocus();
+  expect(screen.getByRole('tabpanel')).toHaveTextContent('Attribute list');
+});
---
+++ packages/components/src/components/common/tabs/tabs.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+import { cn } from '@/utils';
+import { tabsList, tabsTrigger } from './tabs.css';
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = React.forwardRef<React.ElementRef<typeof TabsPrimitive.List>, React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>>(
+  ({ className, ...props }, ref) => <TabsPrimitive.List ref={ref} className={cn(tabsList, className, 'ui-tabs-list')} {...props} />
+);
+TabsList.displayName = TabsPrimitive.List.displayName;
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger ref={ref} className={cn(tabsTrigger, className, 'ui-tabs-trigger')} {...props} />
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+
+const TabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, ...props }, ref) => <TabsPrimitive.Content ref={ref} className={cn(className, 'ui-tabs-content')} {...props} />);
+TabsContent.displayName = TabsPrimitive.Content.displayName;
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
