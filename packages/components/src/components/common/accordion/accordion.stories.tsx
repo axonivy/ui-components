8d1ff9725b94387ae65eb828f0a27a778ac2f094
Lukas Lieb <lukas.lieb@axonivy.com>
2024-09-20 13:53:14 +0200

    Add BasicAccordionItem
    - With messages and control how we use it currently
    - With sticky mode

--- packages/components/src/components/common/accordion/accordion.stories.tsx
+++ packages/components/src/components/common/accordion/accordion.stories.tsx
@@ -1,5 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Accordion, AccordionItem, AccordionContent, AccordionTrigger, AccordionState, type AccordionControlProps } from './accordion';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+  AccordionState,
+  type AccordionControlProps,
+  BasicAccordionItem
+} from './accordion';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { ButtonGroup } from '@/components/common/button/button';
 
@@ -116,4 +124,26 @@ export const Controls: Story = {
       </Accordion>
     );
   }
+};
+
+export const Basic: StoryObj<typeof BasicAccordionItem> = {
+  render: () => (
+    <Accordion type='single' collapsible>
+      <BasicAccordionItem label='Sticky header'>
+        <div style={{ height: '100vh' }}>Very much content, so you need to scroll down to see all.</div>
+      </BasicAccordionItem>
+      <BasicAccordionItem label='With message' state={{ messages: [{ variant: 'warning', message: 'there is a warning in here' }] }}>
+        <div style={{ height: '100vh' }}>Yes. It comes with default styles that matches the other components&apos; aesthetic.</div>
+      </BasicAccordionItem>
+      <BasicAccordionItem
+        label='With controls'
+        controls={[
+          { icon: IvyIcons.ArrowsMaximize, onClick: () => alert('Maximize'), title: 'Maximize' },
+          { icon: IvyIcons.Search, onClick: () => console.log('yey'), title: 'Search', toggle: true }
+        ]}
+      >
+        <div style={{ height: '100vh' }}>Yes. It&apos;s animated by default, but you can disable it if you prefer.</div>
+      </BasicAccordionItem>
+    </Accordion>
+  )
 };
