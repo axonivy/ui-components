64b25afc9419a804c31fa79b24387ed9536912bb
Lukas Lieb <lukas.lieb@axonivy.com>
2024-09-20 15:55:04 +0200

    Add monaco package

---
+++ packages/monaco/src/index.ts
@@ -0,0 +1,11 @@
+export * from './ivy-script-client';
+export * from './monaco-util';
+export * from './monaco-editor-util';
+
+export * from './utils/console-util';
+export * from './utils/promises-util';
+
+export * from './components/CodeEditor';
+export * from './components/SingleLineCodeEditor';
+export * from './components/ResizableCodeEditor';
+export * from './components/monaco-utils';
