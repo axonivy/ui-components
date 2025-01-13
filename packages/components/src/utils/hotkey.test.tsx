24b8711e1bda3c502afee7cc2b6091c579240b12
Lukas Lieb <lukas.lieb@axonivy.com>
2025-01-13 16:18:12 +0100

    Option key

--- packages/components/src/utils/hotkey.test.tsx
+++ packages/components/src/utils/hotkey.test.tsx
@@ -4,11 +4,12 @@ test('hotkeyText', () => {
   expect(hotkeyText('a')).toEqual('a');
   expect(hotkeyText('ctrl+a')).toEqual('ctrl+a');
   expect(hotkeyText('shift+a')).toEqual('⇧+a');
-  expect(hotkeyText('alt+a')).toEqual('alt+a');
+  expect(hotkeyText('alt+a')).toEqual('ALT+a');
   expect(hotkeyText('options+a')).toEqual('options+a');
   expect(hotkeyText('mod+a')).toEqual('CTRL+a');
 
   vi.stubGlobal('navigator', { userAgent: 'Mac' });
   expect(hotkeyText('mod+a')).toEqual('⌘+a');
   expect(hotkeyText('mod+shift+a')).toEqual('⌘+⇧+a');
+  expect(hotkeyText('mod+alt+a')).toEqual('⌘+⌥+a');
 });
