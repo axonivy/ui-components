9691156f802cfd762288667a1582c5a7f5330759
Lukas Lieb <lukas.lieb@axonivy.com>
2025-01-23 08:53:43 +0100

    Fix undo/redo

--- packages/components/src/utils/hotkey.ts
+++ packages/components/src/utils/hotkey.ts
@@ -10,14 +10,14 @@ const optKey = (): string => (isMac() ? '⌥' : 'ALT');
 export const hotkeyText = (hotkey: string): string => hotkey.replace('mod', modKey()).replace('alt', optKey()).replace('shift', '⇧');
 
 export const hotkeyUndoFix = (e: KeyboardEvent, undo: () => void) => {
-  if (isWindows() && e.code !== 'KeyY') {
+  if (isWindows() && e.code === 'KeyZ' && e.key === 'y') {
     return;
   }
   undo();
 };
 
 export const hotkeyRedoFix = (e: KeyboardEvent, redo: () => void) => {
-  if (isWindows() && e.code !== 'KeyZ') {
+  if (isWindows() && e.code === 'KeyY' && e.key === 'z') {
     return;
   }
   redo();
