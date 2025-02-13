4cb1b2813af0158492a9c20740a89c0375a04e4d
ivy-edp <elio.dipuma@axonivy.com>
2025-02-13 17:14:25 +0100

    Add Reactflow-Graph-Component

---
+++ packages/components/src/components/editor/graph/nodes/customNode.css.ts
@@ -0,0 +1,46 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/styles/theme.css';
+
+export const customNode = style({
+  width: '100%',
+  height: '100%',
+  textAlign: 'left',
+  borderRadius: '5px',
+  background: 'white',
+  display: 'inline-block'
+});
+
+export const selectedNode = style({
+  border: `2px solid ${vars.color.p500}`
+});
+
+export const originalNode = style({
+  border: `2px solid ${vars.color.success}`
+});
+
+export const normalNode = style({
+  border: `1px solid ${vars.color.body}`
+});
+
+export const customNodeHeaderLabel = style({
+  fontWeight: 'bold'
+});
+
+export const customNodeHeaderInfo = style({
+  color: `${vars.color.n700}`
+});
+
+export const expander = style({
+  cursor: 'pointer',
+  fontSize: '16px'
+});
+
+export const expanderOpen = style({
+  transform: 'rotate(270deg)',
+  transition: 'transform 200ms cubic-bezier(0.87, 0, 0.13, 1)'
+});
+
+export const expanderClose = style({
+  transform: 'rotate(90deg)',
+  transition: 'transform 200ms cubic-bezier(0.87, 0, 0.13, 1)'
+});
