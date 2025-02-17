f3a97a90462ad7204ba4a6b9d0db145decc98062
ivy-edp <elio.dipuma@axonivy.com>
2025-02-17 14:25:26 +0100

    add better floating edges

--- packages/components/src/utils/graph.ts
+++ packages/components/src/utils/graph.ts
@@ -2,6 +2,7 @@ import type { InternalNode } from '@xyflow/react';
 import { Position } from '@xyflow/react';
 
 type Params = [number, number, Position];
+type IntersectionPoint = { x: number; y: number };
 type EdgeParams = {
   sx: number;
   sy: number;
@@ -79,6 +80,70 @@ export function getEdgeParams(source: InternalNode, target: InternalNode): EdgeP
     sy,
     tx,
     ty,
+    sourcePos,
+    targetPos
+  };
+}
+
+function getNodeIntersection(intersectionNode: InternalNode, targetNode: InternalNode): IntersectionPoint {
+  const { width: intersectionNodeWidth, height: intersectionNodeHeight } = intersectionNode.measured;
+  const intersectionNodePosition = intersectionNode.internals.positionAbsolute;
+  const targetPosition = targetNode.internals.positionAbsolute;
+  if (intersectionNodeWidth === undefined || intersectionNodeHeight === undefined) return { x: 0, y: 0 };
+  const w = intersectionNodeWidth / 2;
+  const h = intersectionNodeHeight / 2;
+
+  const x2 = intersectionNodePosition.x + w;
+  const y2 = intersectionNodePosition.y + h;
+  const x1 = targetPosition.x + (targetNode.measured.width ? targetNode.measured.width / 2 : 0);
+  const y1 = targetPosition.y + (targetNode.measured.height ? targetNode.measured.height / 2 : 0);
+
+  const xx1 = (x1 - x2) / (2 * w) - (y1 - y2) / (2 * h);
+  const yy1 = (x1 - x2) / (2 * w) + (y1 - y2) / (2 * h);
+  const a = 1 / (Math.abs(xx1) + Math.abs(yy1));
+  const xx3 = a * xx1;
+  const yy3 = a * yy1;
+  const x = w * (xx3 + yy3) + x2;
+  const y = h * (-xx3 + yy3) + y2;
+
+  return { x, y };
+}
+
+function getEdgePosition(node: InternalNode, intersectionPoint: IntersectionPoint): Position {
+  const n = { ...node.internals.positionAbsolute, ...node };
+  const nx = Math.round(n.x);
+  const ny = Math.round(n.y);
+  const px = Math.round(intersectionPoint.x);
+  const py = Math.round(intersectionPoint.y);
+
+  if (px <= nx + 1) {
+    return Position.Left;
+  }
+  if (px >= nx + (n.measured.width ? n.measured.width - 1 : 0)) {
+    return Position.Right;
+  }
+  if (py <= ny + 1) {
+    return Position.Top;
+  }
+  if (py >= n.y + (n.measured.height ? n.measured.height - 1 : 0)) {
+    return Position.Bottom;
+  }
+
+  return Position.Top;
+}
+
+export function getCircleEdgeParams(source: InternalNode, target: InternalNode): EdgeParams {
+  const sourceIntersectionPoint = getNodeIntersection(source, target);
+  const targetIntersectionPoint = getNodeIntersection(target, source);
+
+  const sourcePos = getEdgePosition(source, sourceIntersectionPoint);
+  const targetPos = getEdgePosition(target, targetIntersectionPoint);
+
+  return {
+    sx: sourceIntersectionPoint.x,
+    sy: sourceIntersectionPoint.y,
+    tx: targetIntersectionPoint.x,
+    ty: targetIntersectionPoint.y,
     sourcePos,
     targetPos
   };
