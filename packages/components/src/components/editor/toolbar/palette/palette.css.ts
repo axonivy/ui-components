6cfee2c27b500c75d80a747f32e89d68b818136f
Lukas Lieb <lukas.lieb@axonivy.com>
2025-03-28 17:15:25 +0100

    Add palette

---
+++ packages/components/src/components/editor/toolbar/palette/palette.css.ts
@@ -0,0 +1,68 @@
+import { vars } from '@/styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const palette = style({
+  width: 'min(372px, calc(var(--radix-popover-content-available-width) - 2 * var(--size-2)))',
+  maxHeight: 'min(500px, calc(var(--radix-popover-content-available-height) - 2 * var(--size-2)))',
+  overflowY: 'auto'
+});
+
+export const paletteSectionSeparator = style({
+  selectors: {
+    [`${palette} &:last-child`]: {
+      display: 'none'
+    }
+  },
+  flexShrink: 0
+});
+
+export const paletteTitle = style({
+  fontSize: 12,
+  fontWeight: 500,
+  margin: 0
+});
+
+export const paletteItem = style({
+  cursor: 'pointer',
+  fontSize: 10,
+  flexDirection: 'column',
+  padding: 0,
+  border: 'none',
+  color: 'inherit',
+  background: 'inherit',
+  ':focus-visible': {
+    outline: 'none'
+  }
+});
+
+export const paletteItemIcon = style({
+  background: vars.color.n50,
+  borderRadius: vars.border.r2,
+  border: `1px solid ${vars.color.n50}`,
+  height: 32,
+  width: 72,
+  selectors: {
+    [`${paletteItem}:where(:hover, :focus-visible) &`]: {
+      border: `1px solid ${vars.color.body}`
+    }
+  }
+});
+
+export const paletteItemIvyIcon = style({
+  selectors: {
+    [`${paletteItemIcon} &`]: {
+      textAlign: 'center',
+      fontSize: 24
+    }
+  }
+});
+
+export const paletteButtonLabel = style({
+  fontSize: 10,
+  color: vars.color.n900,
+  whiteSpace: 'nowrap'
+});
+
+export const paletteButton = style({
+  gap: '0.125rem'
+});
---
+++ packages/components/src/components/editor/toolbar/palette/palette.test.tsx
@@ -0,0 +1,51 @@
+import { composeStory } from '@storybook/react';
+import { render, screen, userEvent } from 'test-utils';
+import Meta, { Default, CustomPalette, PopoverPalette } from './palette.stories';
+
+const Palette = composeStory(Default, Meta);
+const Custom = composeStory(CustomPalette, Meta);
+const Popover = composeStory(PopoverPalette, Meta);
+
+global.alert = vi.fn();
+
+test('search', async () => {
+  render(<Palette />);
+  expect(screen.getAllByRole('heading')).toHaveLength(3);
+  expect(screen.getAllByRole('button')).toHaveLength(8);
+
+  await userEvent.type(screen.getByRole('textbox'), 'ser');
+  expect(screen.getAllByRole('heading')).toHaveLength(2);
+  expect(screen.getAllByRole('button')).toHaveLength(4);
+  expect(screen.getAllByRole('button')[1]).toHaveAccessibleName('User Task');
+
+  await userEvent.click(screen.getAllByRole('button')[1]);
+  expect(global.alert).toHaveBeenCalledWith('User Task');
+});
+
+test('keyboard', async () => {
+  render(<Palette />);
+  await userEvent.tab();
+  expect(screen.getByRole('textbox')).toHaveFocus();
+
+  await userEvent.tab();
+  expect(screen.getAllByRole('button')[0]).toHaveFocus();
+  await userEvent.keyboard('[Enter]');
+  expect(global.alert).toHaveBeenCalledWith('User Task');
+});
+
+test('custom', async () => {
+  render(<Custom />);
+  expect(screen.getAllByRole('heading')).toHaveLength(3);
+  expect(screen.getAllByRole('button')).toHaveLength(8);
+  expect(screen.getAllByRole('button')[0]).toHaveAccessibleName('Input');
+  expect(screen.getAllByRole('button')[0]).toHaveAttribute('draggable', 'true');
+});
+
+test('popover', async () => {
+  render(<Popover />);
+  await userEvent.click(screen.getByRole('button', { name: 'Default Palette' }));
+  expect(screen.getByRole('button', { name: 'User Task' })).toBeInTheDocument();
+
+  await userEvent.click(screen.getByRole('button', { name: 'Custom Palette' }));
+  expect(screen.getByRole('button', { name: 'Input' })).toBeInTheDocument();
+});
