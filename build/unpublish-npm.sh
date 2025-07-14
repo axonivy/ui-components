6b5191c643140a4748ac671edf1b6a70d66de2c1
Lukas Lieb <lukas.lieb@axonivy.com>
2025-07-14 14:57:51 +0200

    Define some npm configs

--- build/unpublish-npm.sh
+++ build/unpublish-npm.sh
@@ -4,3 +4,8 @@ REGISTRY="https://npmjs-registry.ivyteam.ch/"
 
 npm unpublish "@axonivy/ui-icons@${1}" --registry $REGISTRY
 npm unpublish "@axonivy/ui-components@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/ui-graph@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/jsonrpc@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/eslint-config@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/ts-config@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/prettier-config@${1}" --registry $REGISTRY
