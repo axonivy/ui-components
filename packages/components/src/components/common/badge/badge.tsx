d97ce17edae30765d1f559ab288dfee76a19622d
Lukas Lieb <lukas.lieb@axonivy.com>
2025-07-29 16:35:05 +0200

    Add badge component

---
+++ packages/components/src/components/common/badge/badge.tsx
@@ -0,0 +1,18 @@
+import { badge, type BadgeVariants } from '@/components/common/badge/badge.css';
+import { cn } from '@/utils/class-name';
+import { Slot } from '@radix-ui/react-slot';
+import * as React from 'react';
+
+function Badge({
+  className,
+  variant = 'default',
+  size = 'default',
+  round = false,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'span'> & BadgeVariants & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'span';
+  return <Comp data-slot='badge' className={cn(badge({ variant, size, round }), className, 'ui-badge')} {...props} />;
+}
+
+export { Badge };
