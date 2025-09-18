689a189203850611d38bcff757adbb84ef3fbb0f
Lukas Lieb <lukas.lieb@axonivy.com>
2025-09-18 14:14:04 +0200

    wip

--- packages/components/src/components/common/resizeable/resizable.tsx
+++ packages/components/src/components/common/resizeable/resizable.tsx
@@ -2,13 +2,12 @@ import { IvyIcon } from '@/components/common/icon/icon';
 import { cn } from '@/utils/class-name';
 import { IvyIcons } from '@axonivy/ui-icons';
 import * as ResizablePrimitive from 'react-resizable-panels';
-import { resizableHandle, resizableLine } from './resizable.css';
 
 /**
  * ResizablePanelGroup, based on {@link https://github.com/bvaughn/react-resizable-panels | React Resizable Panels}
  */
 const ResizablePanelGroup = ({ className, ...props }: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
-  <ResizablePrimitive.PanelGroup className={cn(className, 'ui-resizable')} {...props} />
+  <ResizablePrimitive.PanelGroup className={cn('ui-resizable', className)} {...props} />
 );
 
 const ResizablePanel = ResizablePrimitive.Panel;
@@ -19,9 +18,15 @@ export type ResizableHandleProps = React.ComponentProps<typeof ResizablePrimitiv
 
 const ResizableHandle = ({ withHandle, children, className, ...props }: ResizableHandleProps) => (
   <>
-    <ResizablePrimitive.PanelResizeHandle className={cn(resizableLine, className, 'ui-resizable-handle')} {...props}>
+    <ResizablePrimitive.PanelResizeHandle
+      className={cn(
+        'ui-resizable-lane relative flex w-px items-center justify-center bg-n200 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[resize-handle-active]:bg-p300 data-[resize-handle-state=hover]:bg-p300',
+        className
+      )}
+      {...props}
+    >
       {withHandle && (
-        <div className={resizableHandle}>
+        <div className='ui-resizable-handle flex h-4 w-3 items-center justify-center rounded-xs bg-n200'>
           <IvyIcon icon={IvyIcons.EditDots} />
         </div>
       )}
