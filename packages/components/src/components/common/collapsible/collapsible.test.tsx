1a798bc56dd05bf682fbc67c59aff864f96f5e82
Lukas Giger <lukas.giger@axonivy.com>
2025-11-05 08:38:34 +0100

    XIVY-17678 improve open state handling in `BasicCollapsible`
    Do not allow to interact with the state handling of `BasicCollapsible`.
    If the `defaultOpen` prop is set, the collapsible will set its state on mount accordingly.
    If the `defaultOpen` prop is not set, the collapsible will be open on mount if there are state messages present.
    Otherwise, the collapsible will start closed.
    
    If implementors want to have more nuanced open state handling, they should use the underlying components directly.
    Alternatively, we could add both `open` and `onOpenChange` props to `BasicCollapsible` to allow full control over the open state.
    
    Unless I missed something, `BasicCollapsible` has never been used before so this should not break anything.

--- packages/components/src/components/common/collapsible/collapsible.test.tsx
+++ packages/components/src/components/common/collapsible/collapsible.test.tsx
@@ -55,29 +55,19 @@ test('control', async () => {
 describe('basic', () => {
   test.each([
     { props: { state: undefined }, label: 'default' },
-    { props: { state: undefined, open: false }, label: 'open is false' },
     { props: { state: { messages: [] } }, label: 'has no state messages' },
-    { props: { state: undefined, defaultOpen: false }, label: 'defaultOpen is false' },
-    { props: { defaultOpen: true, open: false }, label: 'defaultOpen and has state messages but open is false' }
+    { props: { defaultOpen: false }, label: 'has state messages but defaultOpen is false' }
   ])('starts closed: $label', ({ props }) => {
     customRender(<BasicCollapsible {...props} />);
     expect(screen.getByRole('button', { name: 'Basic' })).toHaveAttribute('data-state', 'closed');
   });
 
   test.each([
-    { props: { state: undefined, open: true }, label: 'open is true' },
     { props: {}, label: 'has state messages' },
-    { props: { state: undefined, defaultOpen: true }, label: 'defaultOpen is true' }
+    { props: { state: undefined, defaultOpen: true }, label: 'has no state but defaultOpen is true' },
+    { props: { defaultOpen: true }, label: 'has no state messages but defaultOpen is true' }
   ])('starts open: $label', ({ props }) => {
     customRender(<BasicCollapsible {...props} />);
-    expect(screen.getByRole('button', { name: 'Basic' })).toHaveAttribute('data-state', 'open');
-  });
-
-  test('change open state', async () => {
-    const view = customRender(<BasicCollapsible state={undefined} open={false} />);
-    expect(screen.getByRole('button', { name: 'Basic' })).toHaveAttribute('data-state', 'closed');
-
-    view.rerender(<BasicCollapsible state={undefined} open={true} />);
     expect(screen.getByRole('button', { name: 'Basic' })).toHaveAttribute('data-state', 'open');
   });
 });
