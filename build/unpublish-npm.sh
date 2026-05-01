4045891e2fd8966d235847c5c73c2aa26f443ac1
Lukas Lieb <lukas.lieb@axonivy.com>
2026-05-01 11:55:48 +0200

    Update to pnpm 11

--- build/unpublish-npm.sh
+++ build/unpublish-npm.sh
@@ -2,10 +2,10 @@
 
 REGISTRY="https://npmjs-registry.ivyteam.ch/"
 
-npm unpublish "@axonivy/ui-icons@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/ui-components@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/ui-graph@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/jsonrpc@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/eslint-config@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/ts-config@${1}" --registry $REGISTRY
-npm unpublish "@axonivy/prettier-config@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/ui-icons@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/ui-components@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/ui-graph@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/jsonrpc@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/eslint-config@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/ts-config@${1}" --registry $REGISTRY
+pnpm unpublish "@axonivy/prettier-config@${1}" --registry $REGISTRY
